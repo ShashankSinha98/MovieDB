290162956cf6b00705ea4785458b9f91535c0e8e
Shashank Sinha <sinhashashank.98@gmail.com>
2022-04-02 17:59:31 +0530

    Setup Home fragment with navigation graph and drawer layout

--- app/src/main/java/com/shashank/moviedb/ui/HomeActivity.java
+++ app/src/main/java/com/shashank/moviedb/ui/HomeActivity.java
@@ -2,37 +2,74 @@ package com.shashank.moviedb.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfigurationKt;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
 import com.shashank.moviedb.R;
 import com.shashank.moviedb.data.ResourceCallback;
 import com.shashank.moviedb.data.Resource;
-import com.shashank.moviedb.data.Status;
-import com.shashank.moviedb.data.remote.MovieApi;
 import com.shashank.moviedb.data.remote.MovieRepository;
-import com.shashank.moviedb.data.remote.MovieRepositoryImpl;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class HomeActivity extends DaggerAppCompatActivity {
+public class HomeActivity extends DaggerAppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "HomeActivity";
 
     @Inject
     public MovieRepository movieRepository;
 
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private NavController navController;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        movieRepository.fetchMovieDetail(634649L, new ResourceCallback() {
-            @Override
-            public void onResponse(Resource resource) {
-                Log.d(TAG, "xlr8: resource: "+resource);
-            }
-        });
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        initNavigation();
+
+    }
+
+    private void initNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, drawerLayout);
     }
 }
---
+++ app/src/main/java/com/shashank/moviedb/ui/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.shashank.moviedb.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.shashank.moviedb.R;
+
+public class HomeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+
+
+}
